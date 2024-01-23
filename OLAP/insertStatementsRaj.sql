935ea7c11f543a63e29bcd9820f49099be29de07
rajkaran27 <rajkaran6969@gmail.com>
2024-01-23 16:59:04 +0800

    insert raj

--- OLAP/insertStatementsRaj.sql
+++ OLAP/insertStatementsRaj.sql
@@ -1,1 +1,26 @@
-INSERT INTO BikeSales
+INSERT INTO BikeSalesDWGroup3..ProductDIM (product_id, categoryKey, brandKey, product_name, model_year, list_price, stock_quantity, stock_date)
+SELECT p.product_id, c.categoryKey, b.brandKey, p.product_name, p.model_year, p.list_price, 0 AS stock_quantity, GETDATE() AS stock_date
+FROM production.products p
+JOIN BikeSalesDWGroup3..CategoryDIM c ON p.category_id = c.category_id
+JOIN BikeSalesDWGroup3..BrandDIM b ON p.brand_id = b.brand_id
+
+// more to fix
+// every product must have stock from all stores, if not should be 0
+
+// chatgpt
+
+INSERT INTO BikeSalesDWGroup3..ProductDIM (product_id, categoryKey, brandKey, product_name, model_year, list_price, stock_quantity, stock_date)
+SELECT p.product_id, c.categoryKey, b.brandKey, p.product_name, p.model_year, p.list_price, ISNULL(s.total_quantity, 0) AS stock_quantity, GETDATE() AS stock_date
+FROM production.products p
+JOIN 
+    BikeSalesDWGroup3..CategoryDIM c ON p.category_id = c.category_id
+JOIN 
+    BikeSalesDWGroup3..BrandDIM b ON p.brand_id = b.brand_id
+LEFT JOIN 
+    (SELECT 
+         product_id, 
+         SUM(quantity) AS total_quantity 
+     FROM 
+         production.stocks 
+     GROUP BY 
+         product_id) s ON p.product_id = s.product_id
