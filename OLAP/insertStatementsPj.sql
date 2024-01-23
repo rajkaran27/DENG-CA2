401722d5958237aca0a6bbd8655a506a64ff7fed
pjcape <pranjalbavikai@gmail.com>
2024-01-23 16:45:34 +0800

    inserted OrderStatusDIM,OrderDIM

--- OLAP/insertStatementsPj.sql
+++ OLAP/insertStatementsPj.sql
@@ -1,3 +1,8 @@
-INSERT INTO BikeSalesDWGroup3..OrderStatusDIM (orderStatus_id, order_status)
+INSERT INTO BikeSalesDWGroup3..OrderStatusDIM (order_status)
 SELECT order_status
 FROM sales.orders;
+
+INSERT INTO BikeSalesDWGroup3..OrderDIM (order_id, orderStatusKey, order_date, required_date, shipped_date)
+SELECT s.order_id, o.orderStatusKey, s.order_date, s.required_date, s.shipped_date
+FROM sales.orders s
+JOIN BikeSalesDWGroup3..OrderStatusDIM o ON s.orderStatusKey = o.orderStatusKey;
