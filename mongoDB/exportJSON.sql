cd80227940a6253e39045971d7cab256f7844d93
pjcape <pranjalbavikai@gmail.com>
2024-01-27 22:13:51 +0800

    fixed mongodb setup db

--- mongoDB/exportJSON.sql
+++ mongoDB/exportJSON.sql
@@ -1,8 +1,24 @@
-unsold
-SELECT p.product_id, p.product_name, b.brand_name,c.category_name,p.model_year,p.list_price FROM production.products p JOIN production.brands b ON p.brand_id = b.brand_id JOIN production.categories c ON c.category_id = p.category_id LEFT JOIN sales.order_items s ON s.product_id = p.product_id WHERE s.product_id IS NULL for JSON path
+--unsold
 
-zerostock
-SELECT p.product_id, p.product_name, b.brand_name,   c.category_name,   p.model_year,  p.list_price, COALESCE(s.quantity, 0) AS quantity FROM production.products p JOIN production.brands b ON p.brand_id = b.brand_id JOIN production.categories c ON c.category_id = p.category_id LEFT JOIN production.stocks s ON s.product_id = p.product_id WHERE COALESCE(s.quantity, 0) = 0 for JSON path
+SELECT p.product_id, p.product_name, b.brand_name,c.category_name,p.model_year,p.list_price 
+FROM production.products p 
+JOIN production.brands b ON p.brand_id = b.brand_id 
+JOIN production.categories c ON c.category_id = p.category_id 
+LEFT JOIN sales.order_items s ON s.product_id = p.product_id 
+WHERE s.product_id IS NULL for JSON path;
 
-stocks
-select p.product_id,s.store_id,s.quantity from production.products p join production.stocks s on s.product_id=p.product_id for JSON path
+-- zerostock
+-- SELECT p.product_id, p.product_name, b.brand_name,   c.category_name,   p.model_year,  p.list_price, COALESCE(s.quantity, 0) AS quantity FROM production.products p JOIN production.brands b ON p.brand_id = b.brand_id JOIN production.categories c ON c.category_id = p.category_id LEFT JOIN production.stocks s ON s.product_id = p.product_id WHERE COALESCE(s.quantity, 0) = 0 for JSON path
+
+SELECT p.product_id, p.product_name, b.brand_name, c.category_name, p.model_year, p.list_price, 
+COALESCE(s.quantity, 0) AS quantity 
+FROM production.products p 
+JOIN production.brands b ON p.brand_id = b.brand_id 
+JOIN production.categories c ON c.category_id = p.category_id 
+LEFT JOIN production.stocks s ON s.product_id = p.product_id 
+WHERE COALESCE(s.quantity, 0) = 0 OR s.product_id is null for JSON path;
+
+--stocks
+--select p.product_id,s.store_id,s.quantity from production.products p join production.stocks s on s.product_id=p.product_id for JSON path
+
+select * from production.stocks where quantity != 0 for JSON path;
