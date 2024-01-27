63342c8de0e48e85b062ec1e818a66a19d5ede4d
chenlexuann <LEXUAN.22@ichat.sp.edu.sg>
2024-01-27 21:57:09 +0800

    Delete OLAP insert statements

--- OLAP/insertStatementsLexuan.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO BikeSalesDWGroup3..CustomerDIM (customer_id, first_name, last_name, phone, email, street, city, state, zip_code)
-SELECT customer_id, first_name, last_name, phone, email, street, city, state, zip_code
-FROM sales.customers;
