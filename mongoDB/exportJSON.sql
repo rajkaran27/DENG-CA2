806c92a02f1b37b7329562a8000eb59bd375d970
rajkaran27 <rajkaran6969@gmail.com>
2024-01-29 11:58:20 +0800

    stocks.json changed

--- mongoDB/exportJSON.sql
+++ mongoDB/exportJSON.sql
@@ -19,6 +19,6 @@ LEFT JOIN production.stocks s ON s.product_id = p.product_id
 WHERE COALESCE(s.quantity, 0) = 0 OR s.product_id is null for JSON path;
 
 --stocks 938
-select p.product_id,s.store_id,s.quantity from production.products p join production.stocks s on s.product_id=p.product_id 
+select p.product_id,s.store_id,s.quantity from production.products p join production.stocks s on s.product_id=p.product_id for JSON path
 
 --select * from production.stocks where quantity != 0 for JSON path;
